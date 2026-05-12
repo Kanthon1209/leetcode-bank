68a8b45709614ed522fcf1e4fb31e756215f05d0
Kanthon <kanthon1209@outlook.com>
2026-05-12 16:19:38 +0800

    update(comment->0041): 缺失的第一个正整数

--- problems/0041-缺失的第一个正整数/java/Solution.java
+++ problems/0041-缺失的第一个正整数/java/Solution.java
@@ -10,14 +10,16 @@ public class Solution {
             while(
                 nums[i] >= 1 &&
                 nums[i] <= nums.length &&
-                nums[nums[i] - 1] != nums[i]
+                nums[nums[i] - 1] != nums[i] // 必须是 正确位置上的元素 不是当前元素, 不能是当前位置上元素不在正确位置
+                // 因为正确位置上的正确元素可能有很多个相同的正确元素, 正确位置上已经是正确元素了就不用再换了
+
+                // 如果用当前位置是正确元素但是不在正确位置, 如果存在多个正确位置正确元素的话, 会无限循环
             ){
                 swap(nums, i, nums[i] - 1); // 前面是当前元素下标, 后面是目标位置下标
             }
         }
-        Arrays.stream(nums).forEach(System.out::println);
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] != i + 1){
+            if(nums[i] != i + 1){ // 如果当前位置的元素和映射元素不同, 那就说明缺失这个元素,  
                 return i + 1;
             }
         }
