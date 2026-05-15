6dbf59bda172dcfa943d83a18fdbedaef77518ff
Kanthon <kanthon1209@outlook.com>
2026-05-15 22:53:56 +0800

    solve(0108): 将有序数组转换为二叉搜索树

---
+++ problems/0108-将有序数组转换为二叉搜索树/java/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    public TreeNode build(int[] nums, int left, int right) {
+        if (left > right)
+            return null;
+        int mid = left + (right - left) / 2; 
+        // 这里是为了方式 left + right 整数溢出, 太细了吧
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = build(nums, left, mid - 1);
+        node.right = build(nums, mid + 1, right);
+        return node; // 左右两边都放好了, 返回, 给更高层的放
+    }
+}
