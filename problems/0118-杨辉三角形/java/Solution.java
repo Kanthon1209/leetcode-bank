f43be5b982ec1985abcebb4f1c308cb9c689cbb4
Kanthon <kanthon1209@outlook.com>
2026-05-27 15:05:12 +0800

    solve(0118): 杨辉三角形 * 有点动态规划的感觉, 当前的结果需要上一次的执行结果, 上一次的执行结果又依赖更加靠前的

---
+++ problems/0118-杨辉三角形/java/Solution.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        new Solution().generate(3);
+    }
+
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < numRows; i++){
+            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j < i + 1; j++){
+                if(j == 0 || j == i){// j == i 表示这行的最后一个位置的下标
+                    row.add(1);
+                }else{
+                    row.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(row);
+        }
+        return res;
+    }
+}
